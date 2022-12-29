dba626996ffeb8aac27476e0b2462c9b2920b0a5
Larry González <larry.g.m1024@gmail.com>
2022-12-29 16:49:56 -0300

    my second commit, add controller UnidadController

---
+++ Controllers/UnidadController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Recetario.Controllers
+{
+    [Route("[controller]")]
+    public class UnidadController : Controller
+    {
+        private readonly ILogger<UnidadController> _logger;
+
+        public UnidadController(ILogger<UnidadController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
